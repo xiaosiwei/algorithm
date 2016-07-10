25fdf1ab53a3451035503d305a57128a4b39381e
xiaosiwei <xiaosiwei@meituan.com>
2016-07-10 22:53:29 +0800

    first pull request

---
+++ src/com/swell/algorithm/leetcode/medium/MinimumPathSum.java
@@ -0,0 +1,34 @@
+package com.swell.algorithm.leetcode.medium;
+
+/**
+ * Created by xiaosiwei on 16/7/10.
+ * Refer to : https://leetcode.com/problems/minimum-path-sum/
+ */
+public class MinimumPathSum {
+    public int minPathSum(int[][] grid) {
+        if (grid.length <= 0) return 0;
+        int[][] result = new int[grid.length][grid[0].length];
+
+        for (int i = 0; i < grid.length; i++) {
+            result[i][0] = i == 0 ? grid[i][0] : (result[i-1][0] + grid[i][0]);
+        }
+
+        for (int i = 0; i < grid[0].length; i++) {
+            result[0][i] = i == 0 ? grid[0][i] : (result[0][i-1]+grid[0][i]);
+        }
+
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 1; j < grid[i].length; j++) {
+                result[i][j] = Math.min(result[i][j-1], result[i-1][j]) + grid[i][j];
+            }
+        }
+
+        return result[result.length-1][result[0].length-1];
+    }
+
+    public static void main(String[] args) {
+        MinimumPathSum minimumPathSum = new MinimumPathSum();
+        int[][] grid = {{1,3,1},{1,5,1},{4,2,1}};
+        System.out.println(minimumPathSum.minPathSum(grid));
+    }
+}

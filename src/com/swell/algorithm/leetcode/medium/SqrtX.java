f3ffd5ce8e0e9c99032e0267da221acfa79b5d0d
xiaosiwei <xiaosiwei@meituan.com>
2016-08-05 13:42:07 +0800

    edit distance

---
+++ src/com/swell/algorithm/leetcode/medium/SqrtX.java
@@ -0,0 +1,11 @@
+package com.swell.algorithm.leetcode.medium;
+
+/**
+ * Created by xiaosiwei on 16/8/5.
+ * Refer to : https://leetcode.com/problems/sqrtx/
+ */
+public class SqrtX {
+    public int mySqrt(int x) {
+        return (int)Math.sqrt(x);
+    }
+}
